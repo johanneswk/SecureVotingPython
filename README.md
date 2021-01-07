aa07a92d855799b5d98a5815f20dad8987ce163d
Johanneswk <johanneswouterk@gmail.com>
2021-01-07 19:10:02 +0100

    Added crash protection and documentation

--- README.md
+++ README.md
@@ -1,21 +1,44 @@
 # SecureVotingPython
 
-Work in progress
+_Work in progress_
+
+This program lets voters defined in `voters.csv` vote on candidates defined in `candidates.csv`. 
+
+To start an election the command `create` needs to be used. All the eligible voters can vote using the `vote` command.
+If the results of the election can be obtained the command `results` needs to be used. 
 
 Required to run:
 - Private key
+- candidates.csv
+- voters.csv
 
 Commands:
-- `vote`
-- `results`
 - `create`
+  - Creates a new election round. Without this command there cannot be voted. This will delete the `vote.state` file,
+    but will keep the anonymized `recount.file` file for recountings.
+- `vote`
+  - Allows an eligible voter to vote using the `-p` (voterID) and `-c` (candidateID) commands. 
+    Results will be stored in encrypted `vote.state` file.
+- `results`
+  - Takes all the data from the encrypted `vote.state` and saves it as `results.txt`.
+  - Sign the result info from inside `results.txt` to `signature.sign`.
+  - Creates a file `recount.file`. This stores all the casted votes individually but without voterID info.
+  - Deletes `vote.state`, but keeps `recount.file` 
 - `delete`
+  - Deletes both `vote.state` and `recount.file`
 
 Example:
+
+`create`
+
 `vote -p 923577756 -c FS`
 
+`results`
+
+`delete`
+
+
 To do:
-- Allow exporting encryption key
 - Create election by certain user
 - Delete election by certain user
 - Error stats (faulty logins, voted twice etc.)
--- voting.py
+++ voting.py
@@ -13,7 +13,9 @@ from secure_delete import secure_delete
 from Crypto.Signature.pkcs1_15 import PKCS115_SigScheme
 from Crypto.Hash import SHA256
 from Crypto.PublicKey import RSA
-from Crypto import Random
+
+
+# from Crypto import Random
 
 
 def key_create():
@@ -56,7 +58,7 @@ def vote(voter, candidate):
                 csv_casted_votes = decrypted_vote_state.split(";", 1)
                 for row_votes_info in csv_casted_votes:
                     if hash_pid in row_votes_info:
-                        print("\nYou aren't eligible to vote or have already voted")
+                        print("You aren't eligible to vote or have already voted")
                         return
 
                 # Add casted vote to temp file
@@ -70,21 +72,19 @@ def vote(voter, candidate):
 
 
 def create():
-    delete()
+    delete(arg="create")
     f = Fernet(mykey)
     encrypted = f.encrypt("hash_pid;canId\n".encode())
 
     with open("vote.state", 'wb') as file:
         file.write(encrypted)
     file.close()
-
     return
 
 
 def results():
     # Publish results on CLI
     decrypted_vote_state = file_decrypt("vote.state").decode().replace("\n", ";")
-    voterlist = decrypted_vote_state.split(";")
 
     votes_emeri = decrypted_vote_state.count("EK")
     votes_frans = decrypted_vote_state.count("FS")
@@ -98,45 +98,83 @@ def results():
         key = RSA.importKey(f.read())
     f.close()
 
-    with open('signer@cs-hva.nl.pub', 'r') as g:
-        pubkey = RSA.importKey(g.read())
-    g.close()
+    # with open('signer@cs-hva.nl.pub', 'r') as g:
+    #     pubkey = RSA.importKey(g.read())
+    # g.close()
 
     h = SHA256.new(result)
     signer = PKCS115_SigScheme(key)
     signature = signer.sign(h)
 
-    with open("results.txt", 'w') as g:
-        g.write(result.decode())
-    g.close()
-
     with open("signature.sign", 'wb') as h:
         h.write(signature)
     h.close()
 
-    # hash = SHA256.new(result)
-    # verifier = PKCS115_SigScheme(pubkey)
-    # try:
-    #     verifier.verify(hash, signature)
-    #     print("Signature is valid.")
-    # except:
-    #     print("Signature is invalid.")
+    with open("results.txt", 'w') as i:
+        i.write(result.decode())
+    i.close()
 
-    delete()
+    # create recountable file
+    recount_file()
+
+    # delete temp voter file after results
+    delete(arg="results")
+
+
+def recount_file():
+    decrypted_vote_state = file_decrypt("vote.state").decode().replace("\n", ";")
+    vote_list = decrypted_vote_state.split(";")
+    anonymized_votes = vote_list[1::2]
+
+    with open('recount.file', 'a+') as f:
+        for anonymized_vote in anonymized_votes:
+            print(anonymized_vote)
+            f.write(anonymized_vote + "\n")
+    f.close()
 
 
 def stats():
     pass
 
 
-def delete():
+def delete(arg):
+    if arg == "delete":
+        try:
+            f = open("vote.state")
+            f.close()
+            secure_delete.secure_random_seed_init()
+            secure_delete.secure_delete("vote.state")
+            secure_delete.secure_delete("recount.file")
+        except IOError:
+            print("Error in deleting files securely")
+
+    elif arg == "start":
+        try:
+            f = open("vote.state")
+            f.close()
+            secure_delete.secure_random_seed_init()
+            secure_delete.secure_delete("vote.state")
+        except IOError:
+            print("First run, creating election...")
+
+    elif arg == "crash" or arg == "results":
+        try:
+            # f = open("vote.state")
+            # f.close()
+            secure_delete.secure_random_seed_init()
+            secure_delete.secure_delete("vote.state")
+        except IOError:
+            print("Error in deleting files at crash")
+
+
+def check_temp_voter_file():
     try:
         f = open("vote.state")
         f.close()
-        secure_delete.secure_random_seed_init()
-        secure_delete.secure_delete("vote.state")
+        return True
     except IOError:
-        print("Vote file does not exist, first run")
+        print("No election created yet. Type '?' for all options")
+        return False
 
 
 if __name__ == '__main__':
@@ -144,14 +182,12 @@ if __name__ == '__main__':
         # Create encryption key
         mykey = key_create()
 
-        # Start new election
-        create()
         # print(file_decrypt("vote.state").decode())
 
         print("Welcome to this electronic voting program!")
         print("Type '?' for all possible arguments")
         while True:
-        # Check user input
+            # Check user input
             try:
                 usr_input = input("> ").split()
             finally:
@@ -173,23 +209,24 @@ if __name__ == '__main__':
                         if not ("-p" and "-c") in usr_input:
                             print("Error, try again")
                         else:
-                            p = usr_input.index('-p')
-                            c = usr_input.index('-c')
-                            persId = usr_input[(p + 1)]
-                            canId = usr_input[(c + 1)]
-                            # print(persId)
-                            # print(canId)
+                            if check_temp_voter_file():
+                                p = usr_input.index('-p')
+                                c = usr_input.index('-c')
+                                persId = usr_input[(p + 1)]
+                                canId = usr_input[(c + 1)]
+                                # print(persId)
+                                # print(canId)
 
-                            # Collect vote
-                            vote(persId, canId)
+                                # Collect vote
+                                vote(persId, canId)
 
-                            # Overwrite persId and canId
-                            persId = os.urandom(16)
+                                # Overwrite persId and canId
+                                persId = os.urandom(16)
 
-                            # print(file_decrypt("vote.state").decode())
+                                # print(file_decrypt("vote.state").decode())
 
                     elif "create" in usr_input[0]:
-                        # Only allow certain user to create an election
+                        # Start new election
                         create()
                     elif "results" in usr_input[0]:
                         # Only allow certain user to publish election results and close election afterwards
@@ -198,13 +235,27 @@ if __name__ == '__main__':
                         # Diagnostic stats?
                         stats()
                     elif "delete" in usr_input[0]:
-                        # Delete all casted votes + diagnostics?
+                        # Delete all casted votes + recount file?
                         print("delete")
-                        delete()
+                        delete(arg="delete")
                     else:
                         print("Type '?' for all possible arguments")
 
 
     except:
         print("Do better testing!")
-        exit()
+
+        try:
+            # Store only anonymized file for counting
+            recount_file()
+
+            # Remove temp file with pii-data
+            # delete(arg="crash")
+            print("Saved crash")
+            # Exit program
+            # exit()
+        except:
+            print("Crashed exit")
+            delete(arg="crash")
+
+
